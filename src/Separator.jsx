8b1d3c0efcf0c68ecbc3fe074d1d55410be428ff
Cruz-Azul <alves.mckl@gmail.com>
2021-12-13 22:24:22 +0100

    📦 Adding first package version

---
+++ src/Separator.jsx
@@ -0,0 +1,13 @@
+import './assets/sass/separator.scss';
+
+import React from 'react';
+
+const separator = {
+	Renderer: () => <hr />,
+	id: 'Separator',
+	title: 'Separator',
+	description: 'Test cell plugin description',
+	version: 1,
+};
+
+export default separator;
--- src/lib/toKebabCase.js
+++
@@ -1,8 +0,0 @@
-function toKebabCase(str) {
-	return str
-		.replace(/([a-z])([A-Z])/g, '$1-$2')
-		.replace(/[\s_]+/g, '-')
-		.toLowerCase();
-}
-
-export default toKebabCase;
