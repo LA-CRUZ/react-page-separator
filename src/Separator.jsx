726ecc3c5b708a48ef2e676b49ec156be123e369
Cruz-Azul <alves.mckl@gmail.com>
2021-12-13 22:35:15 +0100

    🗜 Fix ci for release

--- src/Separator.jsx
+++ src/Separator.jsx
@@ -2,12 +2,16 @@ import './assets/sass/separator.scss';
 
 import React from 'react';
 
-const separator = {
-	Renderer: () => <hr />,
-	id: 'Separator',
-	title: 'Separator',
-	description: 'Test cell plugin description',
-	version: 1,
+// const separator = {
+// 	Renderer: () => <hr />,
+// 	id: 'Separator',
+// 	title: 'Separator',
+// 	description: 'Test cell plugin description',
+// 	version: 1,
+// };
+
+const separator = () => {
+	return <h1>Salut !</h1>;
 };
 
 export default separator;
